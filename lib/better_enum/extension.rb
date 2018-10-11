592a14bbffcf834488397c250e318b9bc7d2f4a7
smoriwaki <smoriwaki@cuon.co.jp>
2018-10-11 13:29:12 +0900

    fix bug around nil about label fetching method.

--- lib/better_enum/extension.rb
+++ lib/better_enum/extension.rb
@@ -13,8 +13,10 @@ module BetterEnum
           enum_values.each_with_object({}) { |(key, value), labels| labels[key] = send(:"#{name}_label", key) }.with_indifferent_access
         end
         _enum_methods_module.module_eval do
-          define_method(:"#{name}_label") { I18n.t((key = send(name).to_sym), default: key.to_s.humanize, scope: [:activerecord, :enums, klass.name.underscore.to_sym, name]) }
-          define_method(:"#{name}_value") { enum_values[send(name)] }
+          define_method(:"#{name}_label") do
+            send(name) && I18n.t((key = send(name).to_sym), default: key.to_s.humanize, scope: [:activerecord, :enums, klass.name.underscore.to_sym, name])
+          end
+          define_method(:"#{name}_value") { send(name) && enum_values[send(name)] }
         end
       end
     end
